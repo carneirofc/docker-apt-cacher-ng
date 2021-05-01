0a32a8919324a9b7b968ec102a0e9b7322b2b5ff
carneirofc <claudiofcarneiro@hotmail.com>
2021-04-30 22:15:19 -0300

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 set -ex
 docker pull debian:buster-slim
-docker build --tag carneirofc/apt-cacher-ng:$(date -I) .
+
+branch=$(git branch --no-color --show-current)
+build_date=$(date -I)
+commit=$(git rev-parse --short HEAD)
+repository=$(git remote show origin |grep Fetch|awk '{ print $3 }')
+tag=carneirofc/apt-cacher-ng:${build_date}
+
+docker build \
+    --label repository.commit=${commit} \
+    --label repository.branch=${branch} \
+    --label repository=${repository} \
+    --label maintainer.name="Claudio F. Carneiro" \
+    --label maintainer.email="claudiofcarneiro@hotmail.com" \
+    --tag ${tag} \
+    .
+
+sed -i "s,image:.*,image: ${tag},g" ./docker-compose.yml
